28e8b44c88a1ba3b400575cf78f9f82e2e6f39bf
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2026-01-09 15:09:05 +0200

    add tostify fix when loging in with admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Login from "./pages/Login.jsx";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
 
   return (
-      <div>
-        <Login/>
-      </div>
+    <div>
+      <ToastContainer />
+      <Login />
+    </div>
   )
 }
 export default App
