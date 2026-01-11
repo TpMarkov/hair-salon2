4b0657ea12a278ce694c1065b8cd92612849da2a
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2026-01-11 12:04:47 +0200

    finish implementing login / register implementation

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import {loginUser, registerUser} from "../controllers/user.controller.js";
+import { loginUser, registerUser, getProfile } from "../controllers/user.controller.js";
+import authUser from "../middleware/authUser.js";
 
 const userRouter = express.Router()
 
 userRouter.post("/register", registerUser)
 userRouter.post("/login", loginUser)
+userRouter.get("/get-profile", authUser, getProfile)
 
 export default userRouter
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -10,6 +10,7 @@ const AppContextProvider = (props) => {
   const [appointments, setAppointments] = useState([])
   const [services, setServices] = useState([])
   const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
+  const [userData, setUserData] = useState(false)
 
 
   const getAppointments = async () => {
@@ -40,17 +41,42 @@ const AppContextProvider = (props) => {
     }
   }
 
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
+      if (data.success) {
+        setUserData(data.userData)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
   useEffect(() => {
     getServicesData()
     getAppointments()
   }, [])
+
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData()
+    } else {
+      setUserData(false)
+    }
+  }, [token])
 
   const value = {
     services,
     backendUrl,
     appointments,
     getAppointments,
-    token, setToken
+    token, setToken,
+    userData, setUserData,
+    loadUserProfileData
   }
 
   return (
